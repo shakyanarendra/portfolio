8b8e18a6b291be8d46efe3ae45b6dcb21c3cc731
narendra <narendrashakyajnv16@gmail.com>
2024-09-14 10:29:09 +0530

    faltu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import Experiences from "./components/Experiences";
 import Skills from "./components/Skills";
 import Project from "./components/Project"; // 
+// narendar bau
 import Contact from "./components/Contact";
 import Aos from "aos";
 import "aos/dist/aos.css";
