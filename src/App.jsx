56a32a9d2de191a9767703728d431d20c90ed3f9
narendra <narendrashakyajnv16@gmail.com>
2024-09-14 13:08:36 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import Home from "./components/Home.jsx";
-import Experiences from "./components/Experiences.jsx";
-import Skills from "./components/Skills.jsx";
-import Project from "./components/Project.jsx"; // 
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Experiences from "./components/Experiences";
+import Skills from "./components/Skills";
+import Project from "./components/Project"; // 
 // narendar bau
-import Contact from "./components/Contact.jsx";
+import Contact from "./components/Contact";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import "./index.css";
